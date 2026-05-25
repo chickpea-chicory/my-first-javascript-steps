72e170b783a84355b846a672ecdb625a2b8e1b99
unknown <nairn036@gmail.com>
2026-05-25 23:23:05 +0100

    chore: remove unrelated game files from repository

--- tracker.py
+++
@@ -1,8 +0,0 @@
-banned_words_lol = ["naughty naughty word_1","lololol!","wordings"]
-
-using_person_message = input("type your message here!")
-
-
-
-if using_person_message in banned_words_lol:
-    print("FILTERED!")
